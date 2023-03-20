8ef26049fb3dfaef8488fc829ec089087da412af
vitu <vitorcsharp@hotmail.com>
2023-03-19 23:50:29 -0300

    criação das pastas - da entidade User - do repository UserRepository - do service UserService

---
+++ src/main/java/dio/diospringsecurityjwt/service/UserService.java
@@ -0,0 +1,26 @@
+package dio.diospringsecurityjwt.service;
+
+import dio.diospringsecurityjwt.model.User;
+import dio.diospringsecurityjwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    //Nossa regra de negocio
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    public void createUser(User user) {
+        String pass = user.getPassword();
+
+        //Criptografando antes de salvar no banco
+        user.setPassword(encoder.encode(pass));
+        repository.save(user);
+    }
+}
